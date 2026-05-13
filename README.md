293207b0eb947e4d70f9d1c2f9fe664922cd54d0
Nicusor Nita <nicusor.nita@gmail.com>
2026-05-13 19:54:48 +0300

    02-06 using a token window strategy

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-### 02-05
+### 02-06
 
-# AiServices under the hood
+# Using a token window strategy
 
-Demonstrating the way AIServices manages under the hood the ChatMemoryProvider.
+Using ChatMemoryProvider with a token window strategy to manage the conversation history in a stateful 
+assistant implementation. 
+
+This version includes an example of how to configure ChatMemoryProvider to limit the conversation history 
+based on the number of tokens rather than the number of messages, providing a more fine-grained control 
+over the memory usage while still maintaining the necessary context for the assistant to generate relevant responses.
+
 
 # Prerequisites
 
--- src/main/java/com/nitan/agenticai/config/LlmConfig.java
+++ src/main/java/com/nitan/agenticai/config/LlmConfig.java
@@ -2,7 +2,8 @@ package com.nitan.agenticai.config;
 
 import com.nitan.agenticai.assistant.ChatAssistant;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
-import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.memory.chat.TokenWindowChatMemory;
+import dev.langchain4j.model.TokenCountEstimator;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.service.AiServices;
@@ -21,25 +22,30 @@ class LlmConfig {
         .baseUrl(BASE_OLLAMA_URL)
         .temperature(0.2)
         .modelName(MODEL)
+        .numCtx(2048) // ⭐
+        .numPredict(512) // ⭐
         .build();
+  }
+
+  @Bean // ⭐
+  TokenCountEstimator estimator() {
+    return new CustomTokenEstimator();
   }
 
   @Bean
   ChatAssistant chatAssistant(ChatModel chatModel) {
     return AiServices.builder(ChatAssistant.class)
         .chatModel(chatModel)
-        .chatMemoryProvider(chatMemoryProvider())//⚠️
+        .chatMemoryProvider(chatMemoryProvider())
         .build();
   }
 
-  @Bean //⭐
+  @Bean
   ChatMemoryProvider chatMemoryProvider() {
-    return memoryId -> {
-      System.out.println("Creating new chat memory for memoryId: " + memoryId);
-      return MessageWindowChatMemory.builder()
-        .id(memoryId)//⚠️
-        .maxMessages(10)
-        .build();
-    };
+    return memoryId ->
+        TokenWindowChatMemory.builder() // ⭐
+            .id(memoryId)
+            .maxTokens(1000, estimator()) // ⚠️
+            .build();
   }
 }
